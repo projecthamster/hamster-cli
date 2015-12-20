a0ad96f9b029fe3710e089dbf267701ce10cd52d
elbenfreund <github.10.elbenfreund@spamgourmet.org>
2015-12-20 09:55:06 +0100

    First basic functinality. Logging. Config.

--- hamster_cli/hamster_cli.py
+++ hamster_cli/hamster_cli.py
@@ -1,6 +1,10 @@
 import click
+import pickle as pickle
 import datetime
-
+import os
+import logging
+from gettext import gettext as _
+import warnings
 
 from hamsterlib import HamsterControl, Category, Activity, Fact
 
@@ -27,6 +31,16 @@ store_config = {
     'dayend': datetime.time(hour=23, minute=59, second=59),
 }
 
+client_config = {
+    'tmp_dir': '.',
+    'tmp_filename': 'tmp_fact.pickle',
+    'log_console': True,
+    'log_file': True,
+    'log_filename': 'hamster_cli.log',
+    'log_console_level': logging.DEBUG,
+    'log_file_level': logging.DEBUG,
+}
+
 class Controler(HamsterControl):
     def __init__(self):
         super(Controler, self).__init__(store_config)
@@ -35,8 +49,9 @@ pass_controler = click.make_pass_decorator(Controler, ensure=True)
 
 
 @click.group()
-def run():
-    pass
+@pass_controler
+def run(controler):
+    __setup_logging(controler.logger)
 
 @run.command()
 @pass_controler
@@ -45,14 +60,38 @@ def list(controler):
 
 
 @run.command()
+@click.argument('raw_fact', default='')
 @pass_controler
-def start(controler):
+def start(controler, raw_fact):
     """
-    Start tracking with *now* as start date.
+    Actually serves two purposes.
+    (1) Add a complete Fact to the db.
+    (2) Start an 'ongoing fact' (possibly backdating start tme.
 
     Should take the raw_fact exluding time info as a parameter.
     """
-    raise NotImplemented
+    # [FIXME]
+    # This should be two different commands!
+
+    fact = controler.parse_raw_fact(raw_fact)
+    if not fact.end:
+        # We seem to want to start a new tmp fact
+        tmp_fact = __load_tmp_fact()
+        if tmp_fact:
+            click.echo(_(
+                "There already seems to be an ongoing Fact present. As there"
+                " can be only one at a time, please use 'stop' or 'camcel' to"
+                " close this existing one before starting a new one."
+            ))
+            controler.logger.debug(_("Trying to start with tmp_fact already present."))
+        else:
+            fact.start = datetime.datetime.now()
+            result = __create_tmp_fact(raw_fact)
+            controler.logger.debug(_("New temporary fact started."))
+    else:
+        # We seem to add a complete fact
+        controler.facts.save(fac)
+        controler.logger.info(_("Fact saved to db."))
 
 
 @run.command()
@@ -61,7 +100,10 @@ def stop(controler):
     """
     Stop tracking current activity
     """
-    raise NotImplemented
+    fact = __load_tmp_fact()
+    fact.end = datetime.datetime.now()
+    result = controler.facts.save(fact)
+    logg
 
 
 @run.command()
@@ -108,6 +150,39 @@ def categories(controler):
 # current()
 # toggle()
 # track()
+
+def __setup_logging(logger):
+    formatter = logging.Formatter(
+        '%(asctime)s [%(levelname)s] %(name)s:  %(message)s')
+
+    if client_config['log_console']:
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(client_config['log_console_level'])
+        logger.addHandler(console_handler)
+
+    if client_config['log_file']:
+        filename = client_config['log_filename']
+        file_handler = logging.FileHandler(filename, encoding='utf-8')
+        file_handler.setLevel(client_config['log_file_level'])
+        file_handler.setFormatter(formatter)
+        logger.addHandler(file_handler)
+
+def __create_tmp_fact(fact):
+    """Create a temporary Fact."""
+    filepath = os.path.join(client_config['tmp_dir'], client_config['tmp_filename'])
+    with open(filepath, 'wb') as fobj:
+        pickle.dump(fact, fobj)
+    return fact
+
+
+def __load_tmp_fact():
+    filepath = os.path.join(client_config['tmp_dir'], client_config['tmp_filename'])
+    with open(filepath, 'rb') as fobj:
+        return pickle.load(fobj)
+
+
+
+
 
 
 
