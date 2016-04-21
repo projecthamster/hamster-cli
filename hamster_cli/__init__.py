0e1833cd98d13b803b2bdec8e42e281ed5a6c47c
elbenfreund <elbenfreund@DenkenInEchtzeit.net>
2016-04-21 22:47:43 +0200

    Add GPL3 boilerplate
    Closes #114

--- hamster_cli/__init__.py
+++ hamster_cli/__init__.py
@@ -1,4 +1,18 @@
-# -*- coding: utf-8 -*-
+# This file is part of 'hamster_cli'.
+#
+# 'hamster_cli' is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# 'hamster_cli' is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with 'hamster_cli'.  If not, see <http://www.gnu.org/licenses/>.
+
 
 __author__ = 'Eric Goller'
 __email__ = 'Elbenfreund@DenkenInEchtzeit.net'
