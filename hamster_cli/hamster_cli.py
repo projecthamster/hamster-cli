d39ae24c3a308740ee8d1ad5c0acd4bfafb823c4
elbenfreund <elbenfreund@DenkenInEchtzeit.net>
2016-04-22 12:29:14 +0200

    Add 'license' command to provide addition information.
    Closes #115

--- hamster_cli/hamster_cli.py
+++ hamster_cli/hamster_cli.py
@@ -489,6 +489,26 @@ def about():
     _launch_window('about')
 
 
+@run.command()
+def license():
+    """Show license information"""
+    license = """
+        'hamster_cli' is free software: you can redistribute it and/or modify
+        it under the terms of the GNU General Public License as published by
+        the Free Software Foundation, either version 3 of the License, or
+        (at your option) any later version.
+
+        'hamster_cli' is distributed in the hope that it will be useful,
+        but WITHOUT ANY WARRANTY; without even the implied warranty of
+        MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+        GNU General Public License for more details.
+
+        You should have received a copy of the GNU General Public License
+        along with .  If not, see <http://www.gnu.org/licenses/>.
+        """
+    click.echo(license)
+
+
 # Helper functions
 def _setup_logging(controler):
     """Setup logging for the lib_logger as well as client specific logging."""
